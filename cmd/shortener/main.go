f942d5862e03994daa3ad24c208cb6b8c5df4977
Anton Kilchik <anton.kilchik@gmail.com>
2022-07-31 16:53:26 +0300

    handle adding urls

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+const addr = "localhost:8080"
+
+func main() {
+	urls := make(map[string]string)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		bb, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		url := string(bb)
+		urls["abc"] = url
+
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	http.ListenAndServe(addr, nil)
+}
